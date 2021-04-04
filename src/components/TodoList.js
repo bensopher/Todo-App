50c083c311383c24e7e5caf8d2f5973853bb4610
BenSopher <bensopher1708@gmail.com>
2021-04-04 10:35:12 +0300

    remove load list button

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,21 @@
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Paper from '@material-ui/core/Paper';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button } from '@material-ui/core';
 
 export default function TodoList() {
 	const [todoList, setTodoList] = useState([]);
+	 
 	
-	const loadList = async () => {
+	useEffect (() => {
 		const response = await fetch('https://nztodo.herokuapp.com/api/tasks/?format=json');
 		const todos = await response.json();
 		setTodoList(todos);
-	}
-	
+	},
+	[]
+	)
+
 	const deleteTodo = async (todoItem) => {
 		await fetch(`https://nztodo.herokuapp.com/api/task/${todoItem.id}?format=json`, {
 			method: 'DELETE'
@@ -22,12 +25,6 @@ export default function TodoList() {
 	
 	return (
 		<Paper className="p-4">
-			<div className="d-flex justify-content-center mb-4">
-				<Button color="primary" variant="contained" onClick={loadList}>
-					Load List
-				</Button>
-			</div>
-			
 			<ul className="list-group">
 				{
 					todoList.map(function(singleTodo) {
