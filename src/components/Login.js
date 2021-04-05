af12b2b696aaa3d70fe59523288ced9bf3496dd2
BenSopher <bensopher1708@gmail.com>
2021-04-05 16:25:13 +0300

    new login component with form validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,20 +5,72 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import Typography from '@material-ui/core/Typography';
 import { useState } from 'react';
 
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
 export default function Login() {
     const [isLoading, setIsLoading] = useState(false);
 
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    })
+
+    const [errors, setErrors] = useState({
+        email: null,
+        password: null
+    })
+
     const handleSubmit = async (event) => {
-        setIsLoading(true);
+
         event.preventDefault();
+
+        // validate the input
+        let isValid = true;
+        let newErrors = { email: null, password: null };
+        if (!values.email) {
+            isValid = false;
+            newErrors = {
+                ...newErrors,
+                email: 'This field is required'
+            }
+        }
+
+        if (!values.password) {
+            isValid = false;
+            newErrors = {
+                ...newErrors,
+                password: 'This field is required'
+            }
+        }
+        if (values.password.length < 5) {
+            isValid = false;
+            newErrors = {
+                ...newErrors,
+                password: 'Minimal length of 5 characters'
+            }
+        }
+        if (!validateEmail(values.email)) {
+            isValid = false;
+            newErrors = {
+                ...newErrors,
+                email: 'Invalid email'
+            }
+        }
+
+        setErrors(newErrors);
+        if (!isValid) {
+            return;
+        };
+
+        setIsLoading(true);
         const response = await fetch(
             'https://academeez-login-ex.herokuapp.com/api/users/login',
             {
                 method: 'POST',
-                body: JSON.stringify({
-                    email: 'yariv@nerdeez.com',
-                    password: '12345678'
-                }),
+                body: JSON.stringify(values),
                 headers: {
                     'Content-Type': 'application/json'
                 }
@@ -27,27 +79,41 @@ export default function Login() {
         const data = await response.json();
         console.log(data);
         setIsLoading(false);
+    }
 
+    const handleChange = (event) => {
+        setValues({
+            ...values,
+            [event.target.name]: event.target.value
+        })
     }
 
     return (
         <Paper className="p-4">
-            <form onSubmit={handleSubmit}>
+            <form noValidate onSubmit={handleSubmit}>
                 <Typography className="text-center" variant="h5">Login</Typography>
                 <div className="form-group">
                     <TextField
+                        error={errors.email}
+                        value={values.email}
+                        onChange={handleChange}
                         className="w-100"
                         name="email"
                         type="email"
-                        label="Enter your email"
+                        label={errors.email ? 'Error' : "Enter your email"}
+                        helperText={errors.email}
                     />
                 </div>
                 <div className="form-group">
                     <TextField
+                        error={errors.password}
+                        value={values.password}
+                        onChange={handleChange}
                         className="w-100"
                         name="password"
                         type="password"
-                        label="Enter your password"
+                        label={errors.password ? 'Error' : "Enter your password"}
+                        helperText={errors.password}
                     />
                 </div>
                 <Button
@@ -55,12 +121,14 @@ export default function Login() {
                     color="primary"
                     disabled={isLoading}
                     type="submit"
-                >Submit
+                >
+                    Submit
 				</Button>
                 {
                     isLoading && <CircularProgress size={24} />
                 }
             </form>
         </Paper>
+
     )
 }
